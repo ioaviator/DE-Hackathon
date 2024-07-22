bca114e0dbbeb81fa5911b249ec835780f93c28a
Ogunseye Oluwajuwon <ogunseyemicheal@gmail.com>
2024-07-22 16:50:17 +0100

    Refactored attach_db_to_motherduck to handle multiple tables efficiently

--- functions.py
+++ functions.py
@@ -7,6 +7,7 @@ from dotenv import load_dotenv
 
 def fetch_country_data(url="https://restcountries.com/v3.1/all"):
     response = requests.get(url)
+    response.raise_for_status()  # Raise an exception for HTTP errors
     countries_data = response.json()
 
     data = {
@@ -30,76 +31,60 @@ def fetch_country_data(url="https://restcountries.com/v3.1/all"):
     }
 
     for i in countries_data:
-        data["Country Name"].append(i.get("name", {}).get("common"))
-        data["Independence"].append(i.get("independent"))
-        data["United Nation members"].append(i.get("unMember"))
-        data["startOfWeek"].append(i.get("startOfWeek"))
-        data["Official country name"].append(i.get("name", {}).get("official"))
+        data["Country Name"].append(i.get("name", {}).get("common", "Unknown"))
+        data["Independence"].append(i.get("independent", "Unknown"))
+        data["United Nation members"].append(i.get("unMember", "Unknown"))
+        data["startOfWeek"].append(i.get("startOfWeek", "Unknown"))
+        data["Official country name"].append(i.get("name", {}).get("official", "Unknown"))
 
         native_names = i.get("name", {}).get("nativeName", {})
-        if native_names:
-            common_native_name = list(native_names.values())[0].get("common")
-        else:
-            common_native_name = None
+        common_native_name = next(iter(native_names.values()), {}).get("common", "Unknown")
         data["Common native name"].append(common_native_name)
 
-        currencies = i.get("currencies")
-        if currencies:
-            currency_codes = ", ".join(currencies.keys())
-            currency_names = ", ".join([details.get("name") for details in currencies.values()])
-            currency_symbols = ", ".join([details.get("symbol") for details in currencies.values() if details.get("symbol")])
-        else:
-            currency_codes = None
-            currency_names = None
-            currency_symbols = None
+        currencies = i.get("currencies", {})
+        currency_codes = ", ".join(currencies.keys())
+        currency_names = ", ".join(details.get("name", "Unknown") for details in currencies.values())
+        currency_symbols = ", ".join(details.get("symbol", "Unknown") for details in currencies.values())
         data["Currency Code"].append(currency_codes)
         data["Currency name"].append(currency_names)
         data["Currency symbol"].append(currency_symbols)
 
         idd = i.get("idd", {})
-        root = idd.get("root", "")
-        suffixes = idd.get("suffixes", [])
-        if root and suffixes:
-            country_code = root + "".join(suffixes)
-        else:
-            country_code = None
-        data["Country code"].append(country_code)
+        country_code = idd.get("root", "") + "".join(idd.get("suffixes", []))
+        data["Country code"].append(country_code or "Unknown")
 
-        capital = i.get("capital", [])
-        if capital:
-            capital_city = capital[0]
-        else:
-            capital_city = None
-        data["Capital"].append(capital_city)
-
-        data["Region"].append(i.get("region"))
-        data["Sub region"].append(i.get("subregion"))
+        data["Capital"].append(i.get("capital", [None])[0])
+        data["Region"].append(i.get("region", "Unknown"))
+        data["Sub region"].append(i.get("subregion", "Unknown"))
 
         languages = i.get("languages", {})
-        if languages:
-            languages_list = ", ".join(languages.values())
-        else:
-            languages_list = None
-        data["Languages"].append(languages_list)
+        languages_list = ", ".join(languages.values())
+        data["Languages"].append(languages_list or "Unknown")
 
-        data["Area"].append(i.get("area"))
-        data["Population"].append(i.get("population"))
+        data["Area"].append(i.get("area", "Unknown"))
+        data["Population"].append(i.get("population", "Unknown"))
 
-        continents = i.get("continents", [])
-        if continents:
-            continent = continents[0]
-        else:
-            continent = None
-        data["Continents"].append(continent)
+        data["Continents"].append(i.get("continents", [None])[0])
 
     df = pd.DataFrame(data)
     return df
 
-def attach_db_to_motherduck():
+def transform_data(df):
+    df.columns = df.columns.str.lower().str.replace(' ', '_')
+    df.fillna('Unspecified', inplace=True)
+
+    df_language = df[['country_name', 'languages']]
+    df_language_expand = df_language.set_index('country_name')['languages'].str.split(', ', expand=True).stack().reset_index(level=1, drop=True).reset_index(name='language')
+    
+    df.drop('languages', axis=1, inplace=True)
+    
+    return df, df_language_expand
+
+def load_data_to_motherduck():
     load_dotenv()
     motherduck_token = os.getenv("MOTHERDUCK_TOKEN")
-    db_path = os.getenv("DB_PATH")
-    db_name = "countries_tour_data"
+    db_name = "my_db"
+
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
     try:
@@ -107,28 +92,29 @@ def attach_db_to_motherduck():
         con = duckdb.connect("md:", config={"motherduck_token": motherduck_token})
         logging.info("Connected to MotherDuck successfully.")
 
-        logging.info(f"Checking if the database '{db_path}' is already attached...")
+        def table_exists(con, db_name, table_name):
+            try:
+                result = con.execute(f"SELECT table_name FROM {db_name}.information_schema.tables WHERE table_name = '{table_name}'").fetchall()
+                return len(result) > 0
+            except Exception as e:
+                logging.error(f"Error checking table existence in MotherDuck: {e}")
+                return False
 
-        attached_dbs = con.execute("PRAGMA database_list").fetchall()
-        logging.info(f"Currently attached databases: {attached_dbs}")
-
-        for db in attached_dbs:
-            if db[1] == db_name:
-                logging.info(f"Detaching the already attached database: {db_name}")
-                con.execute(f"DETACH DATABASE {db_name}")
-                logging.info(f"Database '{db_name}' detached successfully.")
-        logging.info(f"Attaching the database from path: {db_path}")
-        con.sql(f"ATTACH DATABASE '{db_path}' AS {db_name}")
-        logging.info(f"Database '{db_path}' attached successfully.")
-        logging.info(f"Checking if the database '{db_name}' exists in MotherDuck...")
-        existing_dbs = con.execute("SHOW DATABASES").fetchall()
-        if db_name in [db[0] for db in existing_dbs]:
-            logging.info(f"Database '{db_name}' already exists in MotherDuck. Skipping creation.")
+        if not table_exists(con, db_name, 'countries_tour_data'):
+            logging.info("Loading table 'countries_tour_data' into MotherDuck...")
+            con.execute(f"CREATE TABLE {db_name}.countries_tour_data AS SELECT * FROM 'countries_tour_data'")
+            logging.info("Table 'countries_tour_data' loaded successfully.")
         else:
-            logging.info(f"Creating the database '{db_name}' in MotherDuck...")
-            con.sql(f"CREATE DATABASE {db_name} FROM '{db_path}'")
-            logging.info(f"Database '{db_name}' created successfully in MotherDuck.")
+            logging.info("Table 'countries_tour_data' already exists in MotherDuck. Skipping loading.")
+
+        if not table_exists(con, db_name, 'countries_language_expand'):
+            logging.info("Loading table 'countries_language_expand' into MotherDuck...")
+            con.execute(f"CREATE TABLE {db_name}.countries_language_expand AS SELECT * FROM 'countries_language_expand'")
+            logging.info("Table 'countries_language_expand' loaded successfully.")
+        else:
+            logging.info("Table 'countries_language_expand' already exists in MotherDuck. Skipping loading.")
+
     except Exception as e:
-        logging.error(f"Error occurred: {e}")
+        logging.error(f"Failed to load data into MotherDuck: {e}")
     finally:
         con.close()
--- tour_api_ingestion.py
+++ tour_api_ingestion.py
@@ -1,40 +1,117 @@
-import requests
-import pandas as pd
+# import os
+# import logging
+# import duckdb
+# from dotenv import load_dotenv
+# from functions import attach_db_to_motherduck, fetch_country_data, transform_data
+
+# load_dotenv()
+# duckdb_file_path = os.getenv("DB_PATH")
+# logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+# con = None
+# try:
+#     logging.info("Fetching country data from API...")
+#     df, df_language_expand = transform_data(fetch_country_data())
+#     logging.info(f"DataFrame 'df' has {df.shape[0]} rows and {df.shape[1]} columns.")
+#     logging.info(f"DataFrame 'df_language_expand' has {df_language_expand.shape[0]} rows and {df_language_expand.shape[1]} columns.")
+#     logging.info("Data fetched and transformed successfully.")
+
+#     logging.info("Connecting to DuckDB...")
+#     con = duckdb.connect(duckdb_file_path)
+
+#     table1_name = 'countries_tour_data'
+#     logging.info(f"Saving DataFrame to table '{table1_name}' in DuckDB...")
+#     con.execute(f"CREATE OR REPLACE TABLE {table1_name} AS SELECT * FROM df")
+#     logging.info(f"Data saved to table '{table1_name}' successfully.")
+
+#     table2 = 'countries_language_expand'
+#     logging.info(f"Saving DataFrame to table '{table2}' in DuckDB...")
+#     con.execute(f"CREATE OR REPLACE TABLE {table2} AS SELECT * FROM df_language_expand")
+#     logging.info(f"Data saved to table '{table2}' successfully.")
+
+#     tables = con.execute("SHOW TABLES").fetchall()
+#     logging.info(f"Tables in DuckDB: {tables}")
+
+# except Exception as e:
+#     logging.error(f"Error occurred: {e}")
+
+# finally:
+#     if con:
+#         con.close()
+#     logging.info("Disconnected from DuckDB.")
+
+# try:
+#     logging.info("Attaching DuckDB database to MotherDuck...")
+#     attach_db_to_motherduck()
+#     logging.info("Database attached to MotherDuck successfully.")
+# except Exception as e:
+#     logging.error(f"Failed to attach database to MotherDuck: {e}")
 import os
 import logging
 import duckdb
 from dotenv import load_dotenv
-from functions import attach_db_to_motherduck, fetch_country_data, transform_data
+from functions import fetch_country_data, transform_data,load_data_to_motherduck
 
 load_dotenv()
 duckdb_file_path = os.getenv("DB_PATH")
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
+con = None
 try:
     logging.info("Fetching country data from API...")
     df, df_language_expand = transform_data(fetch_country_data())
+    logging.info(f"DataFrame 'df' has {df.shape[0]} rows and {df.shape[1]} columns.")
+    logging.info(f"DataFrame 'df_language_expand' has {df_language_expand.shape[0]} rows and {df_language_expand.shape[1]} columns.")
     logging.info("Data fetched and transformed successfully.")
+
     logging.info("Connecting to DuckDB...")
     con = duckdb.connect(duckdb_file_path)
+
+    def table_exists(con, table_name):
+        try:
+            result = con.execute(f"SHOW TABLES LIKE '{table_name}'").fetchall()
+            return len(result) > 0
+        except Exception as e:
+            logging.error(f"Error checking table existence: {e}")
+            return False
+
     table1_name = 'countries_tour_data'
-    logging.info(f"Saving DataFrame to table '{table1_name}' in DuckDB...")
-    con.execute(f"CREATE TABLE IF NOT EXISTS {table1_name} AS SELECT * FROM df")
-    logging.info(f"Data saved to table '{table1_name}' successfully.")
+    if not table_exists(con, table1_name):
+        logging.info(f"Saving DataFrame to table '{table1_name}' in DuckDB...")
+        con.execute(f"CREATE OR REPLACE TABLE {table1_name} AS SELECT * FROM df")
+        logging.info(f"Data saved to table '{table1_name}' successfully.")
+    else:
+        logging.info(f"Table '{table1_name}' already exists in DuckDB. Skipping ingestion.")
 
     table2 = 'countries_language_expand'
-    logging.info(f"Saving DataFrame to table '{table2}' in DuckDB...")
-    con.execute(f"CREATE TABLE IF NOT EXISTS {table2} AS SELECT * FROM df_language_expand")
-    logging.info(f"Data saved to table '{table2}' successfully.")
+    if not table_exists(con, table2):
+        logging.info(f"Saving DataFrame to table '{table2}' in DuckDB...")
+        con.execute(f"CREATE OR REPLACE TABLE {table2} AS SELECT * FROM df_language_expand")
+        logging.info(f"Data saved to table '{table2}' successfully.")
+    else:
+        logging.info(f"Table '{table2}' already exists in DuckDB. Skipping ingestion.")
+
+    tables = con.execute("SHOW TABLES").fetchall()
+    logging.info(f"Tables in DuckDB: {tables}")
 
 except Exception as e:
     logging.error(f"Error occurred: {e}")
 
 finally:
-    con.close()
+    if con:
+        con.close()
     logging.info("Disconnected from DuckDB.")
+
 try:
     logging.info("Attaching DuckDB database to MotherDuck...")
     attach_db_to_motherduck()
     logging.info("Database attached to MotherDuck successfully.")
 except Exception as e:
     logging.error(f"Failed to attach database to MotherDuck: {e}")
+
+try:
+    logging.info("Loading data into MotherDuck...")
+    load_data_to_motherduck()
+    logging.info("Data loaded into MotherDuck successfully.")
+except Exception as e:
+    logging.error(f"Failed to load data into MotherDuck: {e}")
